c0c0a143a0d9097416d34ba34cb65b614cd88df0
Verchurro <berrygirault@gmail.com>
2023-03-14 11:58:42 +0100

    more

--- Racing/Assets/Core/Scripts/ControlTest.cs
+++ Racing/Assets/Core/Scripts/ControlTest.cs
@@ -7,37 +7,34 @@ using UnityEngine.InputSystem;
 
 public class ControlTest : MonoBehaviour
 {
-
+    [SerializeField] AudioSource bikeNoise;
     Controls _controls;
 
     private Vector2 _moveDirection;
   
     private PlayerInput _inputs;
 
-    public float maxspeed;
-    public float currentspeed;
-    public float currentAcceleration;
-    public float maxRotationSpeed = 1f;
+    Transform rot;
+
+    public float maxspeed, fixedRotation = 180, currentSpeed, currentAcceleration, maxRotationSpeed = 1f;
     public CinemachineBrain brain;
-    public CinemachineVirtualCamera camera1;
-    public CinemachineVirtualCamera camera2;
+    public CinemachineVirtualCamera camera1, camera2;
 
     void Start()
     {
-
         _inputs = GetComponent<PlayerInput>();
 
         InputAction move = _inputs.actions["Move"];
 
-      
-
         move.started += OnMoveStarted;
-        move.performed += OnMoveStarted;
+        move.performed += OnMovePerformed;
         move.canceled+= OnMoveCanceled;
 
         InputAction CameraChange = _inputs.actions["ChangeCam"];
 
         CameraChange.performed += OnChangeCamera;
+
+        rot = transform;
     }
 
     private void OnChangeCamera (InputAction.CallbackContext obj)
@@ -78,6 +75,9 @@ public class ControlTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //put rot y and z at zero when we start
+       
+
         //recupere the donnees of the movements
         float rotationAngle = _moveDirection.x;
         float acceleration = _moveDirection.y;
@@ -85,10 +85,12 @@ public class ControlTest : MonoBehaviour
         // if we accelerate
         if (acceleration == 0)
         {
+            GetComponent<AudioSource>().Play();
             //if were movin and decide to brake
             if (currentAcceleration>0)
             {
                 currentAcceleration -= Time.deltaTime;
+                GetComponent<AudioSource>().Stop();
             }
             else //we backup
             {
@@ -106,17 +108,17 @@ public class ControlTest : MonoBehaviour
 
         if (currentAcceleration >= 0)
         {
-            currentspeed = Mathf.Lerp(0, maxspeed, currentAcceleration);
+            currentSpeed = Mathf.Lerp(0, maxspeed, currentAcceleration);
         }
         else
         {
-            currentspeed = Mathf.Lerp(0, -maxspeed, -currentAcceleration);
+            currentSpeed = Mathf.Lerp(0, -maxspeed, -currentAcceleration);
         }
 
         //influence acceleration on rotation
         rotationAngle = rotationAngle* currentAcceleration* maxRotationSpeed* Time.deltaTime;
 
         transform.Rotate(0, rotationAngle, 0);
-        transform.position = transform.position + transform.forward* (currentspeed *Time.deltaTime);
+        transform.position = transform.position + transform.forward* (currentSpeed *Time.deltaTime);
     }
 }
---
+++ Racing/Assets/Core/Scripts/MotorcycleControlsSimple.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MotorcycleControlsSimple : MonoBehaviour { //MS Motorcycle test - Marcos Schultz (www.schultzgames.com)
+ 
+	public WheelCollider frontWheel;
+	public WheelCollider rearWheel;
+	public GameObject meshFront;
+	public GameObject meshRear;
+ 
+	float rbVelocityMagnitude;
+	float horizontalInput;
+	float verticalInput;
+	float medRPM;
+	
+ 
+	void OnEnable(){
+		WheelCollider WheelColliders = GetComponentInChildren<WheelCollider>();
+		WheelColliders.ConfigureVehicleSubsteps(1000, 30, 30);
+	}
+ 
+	void FixedUpdate () {
+		horizontalInput = Input.GetAxis ("Horizontal");
+		verticalInput = Input.GetAxis ("Vertical");
+		
+		// motorTorque
+		rearWheel.motorTorque = verticalInput * 10f;
+		
+		//steerAngle
+		float nextAngle = horizontalInput * 35.0f;
+		frontWheel.steerAngle = Mathf.Lerp (frontWheel.steerAngle, nextAngle, 0.125f);
+		
+		// Brake
+		if (Mathf.Abs (verticalInput) < 0.1f) 
+		{
+			rearWheel.brakeTorque = frontWheel.brakeTorque =10f;
+			rearWheel.motorTorque = 0.0f;
+		} 
+		else 
+		{
+			rearWheel.brakeTorque = frontWheel.brakeTorque = 0.0f;
+		}
+	}
+ 
+	void Update()
+	{
+		//update wheel meshes
+		Vector3 temporaryVector;
+		Quaternion temporaryQuaternion;
+		
+		frontWheel.GetWorldPose(out temporaryVector, out temporaryQuaternion);
+		meshFront.transform.rotation = temporaryQuaternion;
+		
+		rearWheel.GetWorldPose(out temporaryVector, out temporaryQuaternion);
+		meshRear.transform.rotation = temporaryQuaternion;
+		
+		
+	}
+}
